9bbb983373874f39a2da0b0499cb3d05b73fe894
abdisuhanova <93474979+abdisuhanova@users.noreply.github.com>
2022-04-05 23:51:35 +0600

    Update game of life 2.0.py

--- game of life 2.0.py
+++ game of life 2.0.py
@@ -89,56 +89,19 @@ class Field:
         self.step()
         self.c.after(500, self.draw)
  
-class Player:
-    def __init__(self, c, x, y, size, color="RED"):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.color = color
-        self.c = c
-        self.body = self.c.create_oval(self.x - self.size / 2,
-        self.y - self.size / 2,
-        self.x + self.size / 2,
-        self.y + self.size / 2,
-        fill=self.color)
- 
-    def moveto(self, x, y):
-        self.mx = x
-        self.my = y
-        self.dx = (self.mx - self.x) / 50
-        self.dy = (self.my - self.y) / 50
-        self.draw()
- 
-    def draw(self):
- 
-        self.x += self.dx
-        self.y += self.dy
-        self.c.move(self.body, self.dx, self.dy)
- 
-        print(abs(self.x))
-        if abs(self.mx - self.x) > 2:
-            self.c.after(100, self.draw)
- 
- 
-    def distance(self, x, y):
-        return ((self.x - x)**2 + (self.y - y)**2) ** 0.5
- 
-root = Tk()
-root.geometry("700x700")
-c = Canvas(root, width=700, height=700)
-c.pack()
- 
-f = Field(c, 40, 40, 700, 700)
-f.print_field()
- 
-'''
-p1 = Player(c, 25, 25, 20, "GREEN")
-p2 = Player(c, 375, 25, 20, "RED")
- 
-p1.moveto(150, 200)
-p2.moveto(200, 300)
-'''
 
  
+try:
+    root = Tk()
+    root.geometry("700x700")
+    c = Canvas(root, width=700, height=700)
+    c.pack()
+    f = Field(c, 40, 40, 700, 700)
+    f.print_field()
+ 
+except:
+	print('''Your PC cannnot open the file !!!
+     Use another''')
+ 
+
 root.mainloop()
-# c.create_oval(p.x + 5,p.y + 5,p.x+p.size + 5,p.y+p.size + 5)
